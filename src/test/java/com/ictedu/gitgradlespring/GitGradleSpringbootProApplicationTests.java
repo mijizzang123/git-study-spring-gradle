60dbd12eb3a87cd67225bd3d968ad114b325ff46
ChoiMiJi <meezi_@naver.com>
2024-07-16 15:35:05 +0900

    Git 연습용 프로젝트 생성

---
+++ src/test/java/com/ictedu/gitgradlespring/GitGradleSpringbootProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictedu.gitgradlespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGradleSpringbootProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
