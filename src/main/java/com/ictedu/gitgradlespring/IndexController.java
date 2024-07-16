31d71ef01063522a16c51ea92678bc6d5cf9f41c
ChoiMiJi <meezi_@naver.com>
2024-07-16 15:37:32 +0900

    루트 요청 컨트롤러 생성

---
+++ src/main/java/com/ictedu/gitgradlespring/IndexController.java
@@ -0,0 +1,13 @@
+package com.ictedu.gitgradlespring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
